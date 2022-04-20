8b922b0bcf0b354a322c379b1319a0f40db4143c
Sean Hol <88025815+Sean-Hol@users.noreply.github.com>
2022-04-20 12:58:31 +1000

    Updated main.py with some methods

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,9 @@
+import string
+
 from fastapi import FastAPI
 from typing import Optional
 import mysql.connector
+from pydantic import BaseModel
 
 class Database():
     def __init__(self):
@@ -12,7 +15,7 @@ class Database():
                 port="3306",
                 database="scoreboard"
             )
-        except err:
+        except Exception as err:
             print(f"An error occured connecting to db... trying again")
             return
 
@@ -83,39 +86,82 @@ app = FastAPI()
 
 @app.get("/teams/{tournament_id}")
 #returns a list of Teams
-def handle_scoreboard():
+def handle_scoreboard(tournament_id: string):
     # returns 200: [{
     #   id: string
     #   teamName: string
     #   points: string
     # }]
-    pass
+    cursor = db.database.cursor()
+    result = cursor.execute(f"SELECT Teams.id, Teams.teamName, Teams.points FROM Teams,TournamentTeams WHERE TournamentTeams.tournamentID = {tournament_id} AND TournamentTeams.teamID = Teams.id;")
+    db.database.commit()
+    return result
 
 @app.get("/tasks/{tournament_id}/{team_id}")
-def handle_tasks():
+def handle_tasks(tournament_id: string, team_id: string):
     # returns 200: [{
     #   id: ""
     #   taskName: ""
     #   points: ""
     # }]
-    pass
+    cursor = db.database.cursor()
+    result = cursor.execute(f"SELECT Tasks.id, Tasks.taskName, Tasks.points FROM Tasks,TournamentTeams,TasksCompleted WHERE TournamentTeams.tournamentID = {tournament_id} AND TournamentTeams.teamID = {team_id} AND TournamentTeams.id = TasksCompleted.tournamentTeamID AND TasksCompleted.taskID = Tasks.id;")
+    db.database.commit()
+    return result
 
 @app.post("/tasks/{tournament_id}/{team_id}")
-def handle_post_task():
+def handle_post_task(tournament_id: string, team_id: string, task_id: string, key: string):
     # {
     #   id: ""
     #   key: ""
     # }
     # return 200
     # return 403 (task key doesn't match)
-    pass
+    cursor1 = db.database.cursor()
+    result1 = cursor1.execute(f"SELECT Tasks.key FROM Tasks WHERE id = {task_id};")
+    #"SELECT Tasks.key FROM Tasks,TournamentTeams,TasksCompleted WHERE TournamentTeams.tournamentID = {tournament_id} AND TournamentTeams.teamID = {team_id} AND TournamentTeams.id = TasksCompleted.tournamentTeamID AND TasksCompleted.taskID = Tasks.id;"
+    row = cursor1.fetchone()
+    db.database.commit()
+    if row[0] == key:
+        cursor2 = db.database.cursor()
+        result2 = cursor2.execute(f"SELECT tournamentTeamID FROM TournamentTeams WHERE teamID = {team_id} AND tournamentID = {tournament_id};")
+        row2 = cursor2.fetchone()
+        db.database.commit()
+        tournamentTeam = row2[0]
+        if tournamentTeam != None:
+            cursor = db.database.cursor()
+            result = cursor.execute(f"INSERT INTO TasksCompleted (taskID,tournamentTeamID) Values ({task_id},{tournamentTeam};")
+            db.database.commit()
+            return f"Successfully submitted task"
+        else:
+            return f"Error: Couldn't find team in tournament."
+    else:
+        return f"Incorrect key, try again."
+    
 
 @app.get("/hints/{tournament_id}/{team_id}/{task_id}")
-def handle_hint():
+def handle_hint(tournament_id: string, team_id: string, task_id: string):
     # validate that they have points (if they dont return 403)
     # remove points
     # return 200 {hint: ""}
-    pass
+    cursor = db.database.cursor()
+    result = cursor.execute(f"SELECT points FROM Teams WHERE Teams.id = {team_id};")
+    row = cursor.fetchone()
+    db.database.commit()
+    teamPoints=row[0]
+    if teamPoints>=200:
+        teamPoints = teamPoints - 200
+        cursor1 = db.database.cursor()
+        result = cursor1.execute(f"UPDATE Teams SET points = {teamPoints} WHERE Teams.id = {team_id};")
+        row = cursor1.fetchone()
+        db.database.commit()
+        cursor1 = db.database.cursor()
+        result = cursor1.execute(f"SELECT hint FROM Tasks WHERE Tasks.id = {task_id};")
+        db.database.commit()
+        return result
+    else:
+        return f"This team does not have enough points to get this hint."
+
 
 @app.get("/tournaments")
 def handle_tournaments():
@@ -124,11 +170,14 @@ def handle_tournaments():
     #   id: ""
     #   name: ""
     # }]
-    pass
+    cursor = db.database.cursor()
+    result = cursor.execute(f"SELECT * FROM Tournaments;")
+    db.database.commit()
+    return result
 
 #return end time and team scores for tournament
 @app.get("/tournament/{tournament_id}")
-def handle_tournament():
+def handle_tournament(tournament_id: string):
     #returns 200 {
     # name: "",
     # endTime: "",
@@ -139,7 +188,7 @@ def handle_tournament():
     return result
 
 @app.post("/tournament")
-def handle_post_tournament():
+def handle_post_tournament(name: string, endTime: string):
     # {
     #   name: ""
     #   endTime: ""
@@ -147,24 +196,36 @@ def handle_post_tournament():
     # return { tournament_id }
     try:
         cursor = db.database.cursor()
-        result = cursor.execute("INSERT INTO Tournaments (name, endTime) VALUES ('ooga', 'boonga');")
+        result = cursor.execute(f"INSERT INTO Tournaments (name, endTime) VALUES ({name}, {endTime});")
         db.database.commit()
         return f"Successfully added tournament"
     except Exception as e:
         return f"Internal Server Error: {e}"
 
 @app.patch("/tournament/{tournament_id}")
-def handle_patch_tournament():
+def handle_patch_tournament(team_id: string, tournament_id: string):
     # Add teams to tournament via the teamId
     # {
     #   id: ""
     # }
-    pass
+    try:
+        cursor = db.database.cursor()
+        result = cursor.execute(f"INSERT INTO TournamentTeams (teamID, tournamentID) VALUES ({team_id}, {tournament_id});")
+        db.database.commit()
+        return f"Successfully added team"
+    except Exception as e:
+        return f"Internal Server Error: {e}"
 
 @app.delete("/tournament/{tournament_id}")
-def handle_delete_tournament():
+def handle_delete_tournament(tournament_id: string):
     # delete tournament
-    pass
+    try:
+        cursor = db.database.cursor()
+        result = cursor.execute(f"DELETE from Tournaments WHERE id = {tournament_id});")
+        db.database.commit()
+        return f"Successfully added team"
+    except Exception as e:
+        return f"Internal Server Error: {e}"
 
 @app.post("/register/")
 def handle_register():
